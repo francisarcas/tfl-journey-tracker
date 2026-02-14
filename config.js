8e5a7a8473e33643f0eb41ea7bd133fbcfa682df
francisarcas <qfsn7bd7fm@privaterelay.appleid.com>
2026-02-14 21:21:44 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - DO NOT COMMIT THIS FILE TO GITHUB
+const SUPABASE_CONFIG = {
+    url: "https://yrctiwerbualvmzhvngb.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyY3Rpd2VyYnVhbHZtemh2bmdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA1OTIwMTksImV4cCI6MjA4NjE2ODAxOX0.3Fywb8kgTd0-s24jduK9BNjTtmgijTwM_r6dcbb3RzU"
+};
